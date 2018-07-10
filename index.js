689de2e8e51a3ea3efe2ddfe3358b55ea006cee5
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-09 21:42:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(arr, term){
+  return arr.filter(function(word){return word.toLowerCase() === term.toLowerCase()});
+};
+
+function fuzzyMatch(arr, term){
+  return arr.filter(function(word){return word.startsWith(term)});
+};
+
+function matchName(arr, name){
+  return arr.filter(function(obj){return obj.name === name});
+};
